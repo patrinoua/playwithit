8343e01d19cd6011cb389e7814ce70a88d953e26
Mona Lisa <patrinoua@gmail.com>
2018-08-05 21:31:44 +0200

    lines and sun/moon

--- script.js
+++ script.js
@@ -53,92 +53,69 @@
             opacity: 0.25,
             left: "+=50",
             height: "toggle"
-        }, 2000, function() {
+        }, 1500, function() {
         }).animate({
             opacity: 1,
             left: "+=50",
             height: "toggle"
-        }, 2000, function() {
+        }, 1500, function() {
               console.log('reverse Animation complete');
         });
     })
-    // animation 3
 
+    // animation 3
+    // add the lines
     var horizontalLines = document.getElementById('horizontalLines');
     var horizontalLine;
 
-    // add the lines
     for (var a = 0; a < 7; a++) {
-    horizontalLine = document.createElement('div');
-    horizontalLine.classList.add('horizontalLine');
-    horizontalLine.setAttribute('id', + a);
-    horizontalLines.appendChild(horizontalLine);
+        horizontalLine = document.createElement('div');
+        horizontalLine.classList.add('horizontalLine');
+        horizontalLine.setAttribute('id', 'horizontal'+a);
+        horizontalLines.appendChild(horizontalLine);
     }
-    
+
     var verticalLines = document.getElementById('verticalLines');
     var verticalLine;
     for (var a = 0; a < 7; a++) {
-    vertical = document.createElement('div');
-    vertical.classList.add('verticalLine');
-    vertical.setAttribute('id', + a);
-    grid.appendChild(vertical);
+        verticalLine = document.createElement('div');
+        verticalLine.classList.add('verticalLine');
+        verticalLine.setAttribute('id', 'vertical'+a);
+        verticalLines.appendChild(verticalLine);
     }
 
+    $('#vertical0').css({zIndex:1})
+    $('#vertical2').css({zIndex:1})
+    $('#vertical4').css({zIndex:1})
+    $('#vertical6').css({zIndex:1})
+
+    $('.horizontalLine').on('mouseover',(e)=>{
+        // console.log('horizontalLine mouseover', e.target);
+        $(e.target).css({
+            background:"orange"
+        }).on('mouseout',()=>{
+            $(e.target).css({
+                background:"pink"
+            })
+        })
+    })
+    $('.verticalLine').on('mouseover',(e)=>{
+        // console.log(' vertical mouseover');
+    })
+
+    $('#grid').on('click',(e)=>{
+        // $(e.target).addClass("animateTheLine!")
+        console.log('targeting..', e.target);
+        $(e.target).css({
+            transform: 'rotate(' + +45 + 'deg)'
+        })
+        .on('mouseleave',()=>{
+            $(e.target).css({
+                transform: 'rotate(' + 0 + 'deg)'
+            })
+        })
+    })
+
+    //animation4
+    
 })();
-// var myObj={
-//     section1:0,
-//     section2:0,
-//     section3:0,
-// };
-//
-// myObj[section1] = setInterval(()=>{
-//     var r=Math.floor(Math.random()*256);
-//     var g=Math.floor(Math.random()*256);
-//     var b=Math.floor(Math.random()*256);
-//     console.log('this works');
-//     $(".section1").css({
-//         color: 'rgb('+r+','+g+','+b+')'
-//     });
-// }, 510);
-// console.log('yodfv', myObj);
-// $('.animation1').on('click',function(e){
-//     if(myObj[e.target.className]==0){
-//         myObj[e.target.className] = setInterval(()=>{
-//             var r=Math.floor(Math.random()*256);
-//             var g=Math.floor(Math.random()*256);
-//             var b=Math.floor(Math.random()*256);
-//
-//             $(`.${e.target.className}`).css({
-//                 color: 'rgb('+r+','+g+','+b+')'
-//             });
-//         }, 510);
-//     }else if(myObj[e.target.className]!=0){
-//         clearInterval(myObj[e.target.className])
-//         myObj[e.target.className]=0
-//     }
-// })
-//
-//
-// $('.animation2').click(function(e){
-//     $('.line').animate({
-//         opacity: 0.25,
-//         left: "+=50",
-//         height: "toggle"
-//     }, 3000, function() {
-//     }).animate({
-//         opacity: 1,
-//         left: "+=50",
-//         height: "toggle"
-//     }, 3000, function() {
-//           console.log('reverse Animation complete');
-//     });
-// })
-// $('.line').on('click',function(e){
-//     console.log('liiine',e);
-// }).animate({
-//     opacity: 0.25,
-//     left: "+=50",
-//     height: "toggle"
-//   }, 5000, function() {
-//       console.log('Animation complete');
-// });
